303bebf71742f4d014eb7b5dee05bcb6467c5595
kirandesimone <kirandesimone@gmail.com>
2020-07-21 17:44:17 -0400

    changed the admin file

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -3,9 +3,9 @@ const serviceAccount = require('../socialape.json');
 
 //this is for firebase functions/firestore
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://socialape-9f523.firebaseio.com",
-    storageBucket: "socialape-9f523.appspot.com"
+    credential: null,
+    databaseURL: null,
+    storageBucket: null
 });
 const db = admin.firestore();
 
